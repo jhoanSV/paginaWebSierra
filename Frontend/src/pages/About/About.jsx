e85adf3dc3ebc612909a52c5975e0e279938f4fa
Ger <tec1sierra@gmail.com>
2023-04-24 16:44:48 -0500

    Avance de About
    Avance de About xD

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -6,12 +6,18 @@ export function About() {
     <>
         <div className="about">
             <div className="row">
-                <div className="col d-flex justify-content-center">
-                    imagen bien chida con animación jaja    
-                </div>
-                
-                <div>
-                    Quienes somos?
+                <div className="col">
+                    
+                    <div className="d-flex justify-content-center">
+                        imagen bien chida con animación jaja
+                    </div>
+
+                    <div className="qSomos montserratFont">
+                        <h1>¿Quiénes Somos?</h1>
+                        <span className="font-19">Somos una empresa colombiana con más de 6 Años de experiencia en la industria ferretera con el objetivo de impulsar, fomentar y apoyar al sector ferretero desde la industria nacional e internacional comercializando productos y servicios basándonos en su calidad, respaldo y eficiencia.
+                        Convirtiéndonos en Aliados estratégicos desde la innovación tecnológica y logística buscando que el sector ferretero impulse su proceso de desarrollo empresarial y transformación digital.</span>
+                    </div>
+
                 </div>
             </div>
         </div>
