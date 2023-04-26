84affbc15a0aee1eac5d7aef81bc92c22f62e6ad
Ger <tec1sierra@gmail.com>
2023-04-26 16:49:10 -0500

    Animación de about
    Se hace la animación de texto de about, se empieza a crear la página de privacidad

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./_About.scss";
 
 export function About() {
-  return (
-    <>
-        <div className="about">
-            <div className="row">
-                <div className="col">
-                    
-                    <div className="d-flex justify-content-center">
-                        imagen bien chida con animación jaja
-                    </div>
 
-                    <div className="qSomos montserratFont">
-                        <h1>¿Quiénes Somos?</h1>
-                        <span className="font-19">Somos una empresa colombiana con más de 6 Años de experiencia en la industria ferretera con el objetivo de impulsar, fomentar y apoyar al sector ferretero desde la industria nacional e internacional comercializando productos y servicios basándonos en su calidad, respaldo y eficiencia.
-                        Convirtiéndonos en Aliados estratégicos desde la innovación tecnológica y logística buscando que el sector ferretero impulse su proceso de desarrollo empresarial y transformación digital.</span>
-                    </div>
+    useEffect(() => {
 
+        const qSomosDiv = document.querySelector(".qSomos");
+        qSomosDiv.classList.add("anima-Entrada");
+
+    }, []);
+
+    return (
+        <>
+            <div className="about">
+                <div className="row">
+                    <div className="col">
+                        
+                        <div>
+                            <div className="col log-anima d-flex justify-content-center">
+
+                                <video 
+                                    width="100%"
+                                    height="auto"
+                                    src={require("../../Assets/mp4/ArtemisUjuu.mp4")}
+                                    autoPlay
+                                    muted
+                                />
+
+                            </div>
+                        </div>
+
+                        <div className="qSomos montserratFont">
+                            
+                                <h1>¿Quienes Somos?</h1>
+                                <p className="font-19">Somos una empresa colombiana con más de 6 Años de experiencia en la
+                                 industria ferretera con el objetivo de impulsar, fomentar y apoyar al sector ferretero desde la industria nacional
+                                  e internacional comercializando productos y servicios basándonos en su calidad, respaldo y eficiencia.<br/><br/>
+                                Convirtiéndonos en Aliados estratégicos desde la innovación tecnológica y logística buscando que el sector ferretero 
+                                impulse su proceso de desarrollo empresarial y transformación digital.</p>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
 
-    </>
-  );
+        </>
+    );
 }
---
+++ Frontend/src/pages/Privacy/Privacy.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./_Privacy.scss";
+
+export function Privacy() {
+  return (
+    <>
+        <div>Privacy</div>
+    </>
+  )
+}
