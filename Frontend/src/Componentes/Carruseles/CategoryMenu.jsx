57ee0566e2feb5d7484e1f9b59cb431c689cd65d
Ger <tec1sierra@gmail.com>
2023-05-12 16:57:25 -0500

    Pendiente animación
    Pendiente la animación del menu

--- Frontend/src/Componentes/Carruseles/CategoryMenu.jsx
+++ Frontend/src/Componentes/Carruseles/CategoryMenu.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./_CategoryMenu.scss";
 import categs from "../../Assets/jpg/categorias/categorias.json";
 import { Link } from "react-router-dom";
 
 export function CategoryMenu() {
 
+    const menuCategory = useRef();
+    const menuButton = useRef();
     const items = categs;
 
     const Items = () => (
@@ -26,10 +28,29 @@ export function CategoryMenu() {
         </ul>
     );
 
+    const move = () => {
+        
+        const menuWidth = menuCategory.current.offsetWidth;
+        const contentWidth = menuCategory.current.offsetWidth;
+
+        if (menuWidth === 85) {
+            // Establecer un ancho inicial fijo
+            menuCategory.current.style.maxWidth = "300px";
+            menuCategory.current.style.transition = "700ms"
+            //menuButton.current.style.left = "182px";
+        } else {
+            // Establecer el ancho al valor real del contenido
+            menuCategory.current.style.maxWidth = 85 + "px";
+            //menuButton.current.style.left = "82px";
+        }
+    }
+
+    /*--------------------return----------------------*/
+
     return (
         <div className="container-menu">
-            <div className="menu-category">
-                <div className="menu-button" role="button">
+            <div className="menu-category" ref={menuCategory}>
+                <div className="menu-button" role="button" ref={menuButton} onClick={move}>
                     <i className="bi bi-chevron-left"></i>
                 </div>  
                 <Items/>
