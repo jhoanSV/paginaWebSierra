a020a0c6c1562af096e53a227355d236c4575538
Ger <tec1sierra@gmail.com>
2023-06-05 06:23:05 -0500

    Muchos cambios ahhhh
    Cambios en la navegación del pdf

--- Frontend/src/Componentes/PdfViewer/PdfViewer.jsx
+++ Frontend/src/Componentes/PdfViewer/PdfViewer.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import "./_pdfViewer.scss";
 
 import { Document, Page, Outline, pdfjs } from "react-pdf";
@@ -12,94 +12,117 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 export function PdfViewer({ prop }) {
 
     const [numPages, setNumPages] = useState(null);
-    const [bookmark, setBookmark] = useState(1);
-    const [pageNumber, setPageNumber] = useState(1);
+    const [bookmark, setBookmark] = useState();
+    const [lengthArr,setLengthArr] = useState(9);
+    //const [pageNumber, setPageNumber] = useState(1);
+    const [catPage, setCatPage] = useState([]);
 
-    const [catPag, setCatPage] = useState([]);
+    var chrg = 4; 
 
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
     };
 
     useEffect(() => {
-        var chrg = 4;
-        if(bookmark<5);
-        setPageNumber(bookmark);
+        //setPageNumber(bookmark);
+        if(bookmark < 5) chrg = bookmark - 1 ;
 
         const initialArray = [];
         for (let i = 0; i < 9; i++) {
             initialArray.push(<Page
                 className={"pagePdf"}
-                pageNumber={(bookmark - 4) + i}
+                pageNumber={(bookmark - chrg) + i}
+                width={560}
             />);
         }
         setCatPage(initialArray);
     },[bookmark]);
-
-    function changePage(offset) {
-        setPageNumber(prevPageNumber => prevPageNumber + offset);
-    }
     
     function previousPage() {
-        const newPage = <Page
-            className={"pagePdf"}
-            pageNumber={82}
-        />;
-        setCatPage([newPage,...catPag]);
+        const nPage = bookmark - lengthArr + chrg;
+        const array = [];
+        const item = document.querySelector('.pagesContainer');
+        var posX = (parseFloat(item.style.left));
+
+        if(nPage <= 2){
+            return;
+        }else if(posX === -100){
+            item.style.left = (posX - 100) + "%";
+            item.style.transition = '500ms';
+            for (let i = 3; i >= 0; i--) {
+                array.push(<Page
+                    className={"pagePdf"}
+                    pageNumber={nPage - i}
+                    width={560}
+                />);
+                setCatPage([array,...catPage]);
+            }
+            setLengthArr(lengthArr + 4);
+        }else{
+            item.style.left = (posX + 100) + "%";
+            item.style.transition = '2000ms';
+        }
+        console.log("2-->" + lengthArr);
         //changePage(-2);
     }
     
     function nextPage() {
-        const newPage = <Page
-            className={"pagePdf"}
-            pageNumber={82}
-        />;
-        setCatPage([...catPag, newPage]);
-        //changePage(2);
+        const nPage = bookmark + lengthArr - chrg;
+        const array = [];
+        const item = document.querySelector('.pagesContainer');
+        var posX = (parseFloat(item.style.left));
 
+        if(nPage >= numPages-1){
+            return;
+        }else if(-posX === (parseInt(lengthArr/2)-1)*100){
+            item.style.left = (posX - 100) + "%";
+            for (let i = 0; i < 4; i++) {
+                array.push(<Page
+                    className={"pagePdf"}
+                    pageNumber={nPage + i}
+                    width={560}
+                />);
+                setCatPage([...catPage,array]);
+            }
+            setLengthArr(lengthArr + 4);
+        }else{
+            item.style.left = (posX - 100) + "%";
+            item.style.transition = '2000ms';
+        }
+        console.log("2-->" + lengthArr);
     }
 
     /*------------------------------------------*/
     return (
+        <>
+            <Document className={"rctPdf"} file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+                <Outline className={"hide-outline"} onLoadSuccess={ (outline) => {
+                        const outlines = outline.filter(item => item.title === prop);
+                        outlines.map((item) => setBookmark(item.dest[0]+1));
+                    }}
+                />
 
-    <Document className={"rctPdf"} file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
-        <Outline className={"hide-outline"} onLoadSuccess={ (outline) => {
-                const outlines = outline.filter(item => item.title === prop);
-                outlines.map((item) => setBookmark(item.dest[0]+1));
-            }}
-        />
-        {/*<Page className={"pagePdf"} pageNumber={pageNumber}/>
-        <Page  className={"pagePdf"} pageNumber={pageNumber+1}/>*/}
+                {bookmark && <div className='pagesContainer'
+                    style={ bookmark < 5 ? { left: '0%' } : {left: '-200%'}}>
+                    {
+                        catPage.map((component, index) => (
+                            <div className='d-flex' key={index}>
+                                {component}
+                            </div>
+                        ))
+                    }
+                </div>}
 
-        {/*Array.from(
-            new Array(9),
-            (el, index) => (
-                <>
-                    <Page
-                        key={`page_${index + 1}`}
-                        className={"pagePdf"}
-                        pageNumber={(pageNumber)}
-                    />
-                </>
-            ),
-        )*/
-        }
+                {<button onClick={previousPage} className="prevPdf">
+                    <i className="bi bi-arrow-left-circle-fill"></i>
+                </button>}
 
-        {
-            catPag.map((component, index) => (
-                <div key={index}>{component}</div>
-            ))
-        }
+                {
+                <button onClick={nextPage} className="nextPdf">
+                    <i className="bi bi-arrow-right-circle-fill"></i>
+                </button>}
 
-        {<button onClick={previousPage} className="prevPdf">
-            <i className="bi bi-arrow-left-circle-fill"></i>
-        </button>}
-
-        {
-        <button onClick={nextPage} className="nextPdf">
-            <i className="bi bi-arrow-right-circle-fill"></i>
-        </button>}
-
-    </Document>
+            </Document>
+        </>
   )
 }
