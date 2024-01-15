e3bf92cb8996f25333af565077d8d95332e3f2e3
TecS1 <tec1sierra@gmail.com>
2024-01-15 16:53:17 -0500

    Se cambia el catalogo y se agrega vinculo en home de contactanos

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -69,7 +69,7 @@ export function Home() {
                     <div className="row g-0 avi fs-2">
                         <div className="col ">
                             <div className="d-flex justify-content-center gx-1">
-                                <p>Eres <span> Ferretero? </span> tenemos precios especiales para ti... <span>contactanos</span></p>
+                            <p>Eres <span> Ferretero? </span> tenemos precios especiales para ti... <Link to="/contactUs"><span>contactanos</span></Link></p>
                             </div>
                         </div>
                     </div>
@@ -206,7 +206,7 @@ export function Home() {
 
                                 <div className="col">
                                     <div className="btn-catalogo1">
-                                        <a href={require("../../Assets/docs/Catalogo.pdf")} download>
+                                        <a href={require("../../Assets/docs/Catalogo2024.pdf")} download>
                                             <div className='d-flex align-items-center'>
                                                 <img
                                                     src={require("../../Assets/png/DescargaCatalogo2.png")}
