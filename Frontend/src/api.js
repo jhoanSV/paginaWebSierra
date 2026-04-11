be23854c5d31c93da48bf2eeb97d47b23c5c344c
jhoanSV <jhoanjssv97@outlook.com>
2026-04-11 11:33:37 -0500

    Coneccion con api de pruebas

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,8 +1,8 @@
 // const API = 'http://000.000.0.000:0000/tasks';
 //const API = process.env.REACT_APP_API;
 //const API = 'http://192.168.1.110:3000/tasks';
-const API = 'http://192.168.101.13:3000/tasks';
-
+//const API = 'http://192.168.101.13:3000/tasks';
+const API = 'http://44.209.105.117/tasks'; //URL de pruebas remoto
 
 //const API = 'http://localhost:3000/tasks';
 
