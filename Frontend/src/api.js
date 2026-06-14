8e8ee8f39b1131ab2b5f16165bc7be6aee69ce55
JhoanSSV <jhoanjssv97@outlook.com>
2026-06-14 11:49:46 -0500

    Quitar el build de la carpeta

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,10 +1,10 @@
 // const API = 'http://000.000.0.000:0000/tasks';
-//const API = process.env.REACT_APP_API;
+const API = process.env.REACT_APP_API;
 //const API = 'http://192.168.1.110:3000/tasks';
 //const API = 'http://192.168.101.17:3000/tasks'; //My computer
 //const API = 'http://44.209.105.117/tasks'; //URL de pruebas remoto
 
-const API = 'https://sivar.com.co/tasks';
+//const API = 'https://sivar.com.co/tasks';
 
 export const validateUser = async (validateValueUser) => {
     /*Validate the user information and if it's correct return the data of the user
